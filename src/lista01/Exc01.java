386d241603b3bdfffa5bdb57d7760a190b29b356
giova <giovanaximenes14@gmail.com>
2024-06-21 14:14:46 -0300

    Lista01 Exercício01

---
+++ src/lista01/Exc01.java
@@ -0,0 +1,20 @@
+package lista01;
+
+import java.util.Scanner;
+
+public class Exc01 {
+
+    public static void main(String[] args) {
+        float salario, abono, novoSal;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Insira seu salário: ");
+        salario = leia.nextFloat();
+        System.out.println("Insira o abono: ");
+        abono = leia.nextFloat();
+
+        novoSal = salario + abono;
+
+        System.out.println("Seu novo salário é de: " + novoSal);
+    }
+}
